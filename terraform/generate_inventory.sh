162f1e5e4138f36068249d25fcca06c7ab5a0d44
Ram ratan yadav <rratan100@gmail.com>
2024-12-30 02:03:46 +0530

    Update generate_inventory.sh

--- terraform/generate_inventory.sh
+++ terraform/generate_inventory.sh
@@ -8,4 +8,4 @@ all:
       ansible_host: $(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Bastion-Host' --query 'Reservations[].Instances[].PublicIpAddress' --output text)
       ansible_user: ubuntu
       ansible_ssh_private_key_file: /var/lib/jenkins/tokyojenkins.pem
-" > inventory.yml
+" > ../ansible/inventory.yml

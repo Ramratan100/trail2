a9d09cf6abe5b25dbb862221c7805fd0156fc3b5
Ram ratan yadav <rratan100@gmail.com>
2025-01-11 17:50:07 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,6 @@ variable "region" {
 }
 
 variable "database_vpc_cidr" {
-  #default = "10.0.0.0/16"
   type    = string
 }
 
@@ -14,62 +13,50 @@ variable "master_vpc_cidr" {
 }
 
 variable "master_route_table_id" {
-  #default = "rtb-0c82564b54a7fa492"
   type    = string
 }
 
 variable "master_vpc_id" {
   description = "Master VPC ID"
-  #default     = "vpc-00ec09536f7ae310f"
   type        = string
 }
 
 variable "public_subnet_cidr" {
-  #default = "10.0.2.0/24"
   type    = string
 }
 
 variable "private_subnet_cidr" {
-  #default = "10.0.1.0/24"
   type    = string
 }
 
 variable "az" {
-  #default = "ap-northeast-1a"
   type    = string
 }
 
 variable "bastion_subnet_cidr" {
-  #default = "10.0.2.0/24"
   type    = string
 }
 
 variable "mysql_subnet_cidr" {
-  #default = "10.0.1.0/24"
   type    = string
 }
 
 variable "bastion_ami" {
-  #default = "ami-0ac6b9b2908f3e20d"
   type    = string
 }
 
 variable "mysql_ami" {
-  #default = "ami-0ac6b9b2908f3e20d"
   type    = string
 }
 
 variable "bastion_instance_type" {
-  #default = "t2.micro"
   type    = string
 }
 
 variable "mysql_instance_type" {
-  #default = "t2.micro"
   type    = string
 }
 
 variable "key_name" {
-  #default = "tokyojenkins"
   type    = string
 }
